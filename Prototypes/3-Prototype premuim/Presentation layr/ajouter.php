1b184a5436c774017f2ee918b025998347c1323a
stitou-nada <nadastito1087@gmail.com>
2022-10-12 13:17:37 +0100

    alert search

--- Prototypes/3-Prototype premuim/Presentation layr/ajouter.php
+++ Prototypes/3-Prototype premuim/Presentation layr/ajouter.php
@@ -1,5 +1,5 @@
 <?php
-  include ("Business/PromotionBLL.php");
+  include ("../Business/PromotionBLL.php");
  $gestionPromotions = new PromotionDA();
 
 
---
+++ Prototypes/3-Prototype premuim/Presentation layr/index.php
@@ -0,0 +1,61 @@
+<?php
+
+
+include ("../Business/PromotionBLL.php");
+ $PromotionBLL = new PromotionBLL;
+ $data = $PromotionBLL->AfficherPromotions();
+ 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>gestion promotion</title>
+</head>
+<body>
+    <div class="container" style="max-width: 50%;">
+    <div class="text-center mt-5 mb-4">
+         <h2>GESTION PROMORION</h2>
+         <a href="ajouter.php"> Ajouter une promotion</a>
+    </div>
+    
+    <input type="text" class="form-control" id="live_search" autocomplete="off"
+    placeholder="Search...
+    "></input>
+
+   
+    <table >
+     <tr>
+        <th>Id</th>
+        <th>Nom de promotion</th>
+     </tr>
+     <?php 
+       foreach($data as $value){
+    ?>
+      <tr>
+         <td><?= $value->getId() ?></td>
+         <td><?= $value->getNom() ?></td>
+         <td>
+                    <a href="modifier.php?id=<?php echo $value->getId() ?>">Modifier</a>
+                    <a href="supprimer.php?id=<?php echo $value->getId() ?>">Supprime</a>
+         </td>
+      </tr>
+     
+     <?php }?>
+    </table>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="text/javascript">
+               $(document).ready(function(){
+                  $("#live_search").keyup(function(){
+                     var input = $(this).val();
+                     alert(input);
+                  });
+               });
+    </script>
+</body>
+</html>
