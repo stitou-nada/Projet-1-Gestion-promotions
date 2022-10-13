7cf5f5102ac1038a96a532a33b3ba00a546d0f0f
stitou-nada <nadastito1087@gmail.com>
2022-10-13 11:40:13 +0100

    search

--- Realisation/assest/js/search.js
+++ Realisation/assest/js/search.js
@@ -1,20 +1,18 @@
-
-type="text/javascript">
-               $(document).ready(function(){
-                  $("#live_search").keyup(function(){
-                     var input = $(this).val();
-                     // alert(input);
-                     if(input!= " " ){
-                        $.ajax({
-                           url:"../Data_access/livesearch.php",
-                           method:"POST",
-                           data:{input:input},
-
-                           success:function(data){
-                              $("#searchresult").html(data);
-                           }
-                        }); 
-                     }
-                  });
-               });
-  
+$(document).ready(function () {
+   $("#live_search").keyup(function () {
+      var input = $(this).val();
+      // alert(input);
+      if (input != " ") {
+         $.ajax({
+            url: "../Data_access/livesearch.php",
+            method: "POST",
+            data: {input: input },
+           
+            
+            success: function (data) {
+               $("#searchresult").html(data);
+            }
+         });
+      }
+   });
+});
